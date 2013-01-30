b6c4f4dfaf62439207bcfeda1954058335cbf180
Satu Kallio <satu.kallio@lut.fi>
2013-01-30 23:28:43 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   describe "when name is not present" do
@@ -108,6 +109,11 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
