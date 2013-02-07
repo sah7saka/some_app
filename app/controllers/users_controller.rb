8b8758e9a726a1910cc52996630919932c6bf3f3
Satu Kallio <satu.kallio@lut.fi>
2013-02-07 11:09:28 +0200

    Finish layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to LUT SoMe!"
+      flash[:success] = "Welcome to SO:ME!"
       redirect_to @user
     else
       render 'new'
